14cc6f03b6858a9fe960d8a4c2f7b6421af7427c
UPPU PALAKONDA RAJU <98731451+raju20215@users.noreply.github.com>
2025-08-19 23:22:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.compose import ColumnTransformer
